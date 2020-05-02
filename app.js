891c0a94b7c37c2ba8a43458c548c74ef1bb36f5
Akshay Kumar <akshay764608@gmail.com>
2020-05-02 12:38:25 +0530

    Hashing

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
@@ -23,9 +24,9 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-/////////////Level 2 Security////////////
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
-///////////over//////////
+// /////////////Level 2 Security////////////
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+// ///////////over//////////
 
 
 const User = mongoose.model("User", userSchema);
@@ -46,7 +47,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err) {
     if(err){
@@ -59,7 +60,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser) {
     if(err) {
