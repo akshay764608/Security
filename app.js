8d10e874be372f822d5f267918f987cff01c0c43
Akshay Kumar <akshay764608@gmail.com>
2020-05-03 15:47:59 +0530

    Complete Authentication and security without o-auth

--- app.js
+++ app.js
@@ -4,13 +4,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
@@ -21,8 +31,11 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
 
 const userSchema = new mongoose.Schema ({
   email: String,
-  password: String
+  password: String,
+  secret: String
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 // /////////////Level 2 Security////////////
 // userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
@@ -30,6 +43,11 @@ const userSchema = new mongoose.Schema ({
 
 
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/", function(req, res) {
@@ -44,38 +62,82 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
-  newUser.save(function(err) {
-    if(err){
+app.get("/secrets", function(req, res) {
+
+  User.find({"secret": {$ne: null}}, function(err, foundUsers) {
+    if(err) {
       console.log(err);
     } else {
-      res.render("secrets");
+      if(foundUsers) {
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  });
+
+});
+
+app.get("/submit", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("submit");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/submit", function(req, res) {
+  const submittedSecret = req.body.secret;
+
+  User.findById(req.user.id, function(err, foundUser) {
+    if(err) {
+      console.log(err);
+    } else {
+      if(foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function() {
+          res.redirect("/secrets");
+        });
+      }
     }
   });
 });
 
-app.post("/login", function(req, res) {
-  const username = req.body.username;
-  const password = md5(req.body.password);
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
 
-  User.findOne({email: username}, function(err, foundUser) {
+app.post("/register", function(req, res) {
+
+  User.register({username: req.body.username}, req.body.password, function(err, user) {
     if(err) {
       console.log(err);
-    } else{
-      if(foundUser) {
-        if(foundUser.password === password) {
-          // console.log(foundUser.password);
-          res.render("secrets");
-        } else {
-          res.sendFile(__dirname + "/passwordFailure.html");
-        }
-      }
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
     }
   });
+
+});
+
+app.post("/login", function(req, res) {
+
+  const user = new User ({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err){
+    if(err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
+    }
+  });
+
 });
 
 
@@ -83,4 +145,4 @@ app.post("/login", function(req, res) {
 
 app.listen("3000", function() {
   console.log("Server has started on port 3000.");
-})
+});
